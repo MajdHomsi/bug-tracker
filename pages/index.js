8c3d5c812e3970019b8e1d1fcbed3c07a3923559
seewilds <wildmanc@hotmail.com>
2022-08-21 23:56:56 -0400

    list of bugs

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,12 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         Bug Tracker
         </h1>
+        <ul>
+        <li>COVID</li>
+        <li>Monkeypox</li>
+        </ul>
 
         <p className={styles.description}>
           Get started by editing{' '}
